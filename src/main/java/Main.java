714ee48d8ba032d5e2a148d02fb5fd17879dcd54
Victor Revenco <victor.revenco@isa.utm.md>
2025-05-04 10:02:23 +0300

    Add JSOUP usage in code and refactor for readability

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,10 @@ import factory.WebDriverFactory;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
+        String base_url = "https://999.md";
+        String car_brand = "Renault";
+        String car_model = "Megane";
+        String car_generation = "III (2008 - 2016)";
         String choice = "Chrome";
         WebDriverFactory factory;
         Scraper scraper;
@@ -12,7 +16,7 @@ public class Main {
         } else {
             factory = new FirefoxDriverFactory();
         }
-        scraper = new Scraper(factory);
+        scraper = new Scraper(factory, base_url, car_brand, car_model, car_generation);
         scraper.scrape();
     }
 }
